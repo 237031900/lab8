8bb2ba3b45c1ac0cd501e926f678c148952efd68
237031900 <155444966+237031900@users.noreply.github.com>
2024-02-29 21:18:53 +0800

    chit ref

---
+++ config.ts
@@ -0,0 +1,10 @@
+export const config = {
+    host: "",
+    port: 5432,
+    user: "",
+    password: "",
+    database: "",
+    connection_limit: 100
+}
+
+// postgres://xnnzzunw:HxhREpzPFo5H5gzn2tukvsrfJhKEfP4p@rain.db.elephantsql.com/xnnzzunw
